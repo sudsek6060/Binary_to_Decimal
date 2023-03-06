fbc65c728f810b4cce3bfbad7d4298e5db5f5420
sudsek6060 <sekhar.sudhansu6060@gmail.com>
2023-03-06 18:14:15 +0530

    Binary to decimal

--- app.js
+++ app.js
@@ -4,5 +4,16 @@ const decimal = document.querySelector('.result');
 
 
 const convert = () => {
-
+    const other = [2,3,4,5,6,7,8,9]
+    const val = binary.value;
+    const len = val.length;
+    let des = 0;
+    for (let i = 0; i < len; i++) {
+        des = des + val[i] * (2**((len - 1) - i))
+    }
+     decimal.textContent = `Decimal No : ${des}`
 }
+btn.addEventListener('click', function(e){
+    e.preventDefault();
+    convert()
+})
