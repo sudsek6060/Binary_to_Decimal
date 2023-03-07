18e218e788358f2f9e545f827ef7a9e18b6ce444
sudsek6060 <sekhar.sudhansu6060@gmail.com>
2023-03-07 18:26:31 +0530

    error handle

--- app.js
+++ app.js
@@ -4,16 +4,23 @@ const decimal = document.querySelector('.result');
 
 
 const convert = () => {
-    const other = [2,3,4,5,6,7,8,9]
+    const others = [2,3,4,5,6,7,8,9]
     const val = binary.value;
     const len = val.length;
     let des = 0;
-    for (let i = 0; i < len; i++) {
-        des = des + val[i] * (2**((len - 1) - i))
+    const check = others.map(e => val.includes(e));
+    
+    if (check.includes(true)) {
+        alert('Number must be in 1 and 0 format');
+    } else {
+        for (let i = 0; i < len; i++) {
+            des = des + val[i] * (2 ** ((len - 1) - i))
+        }
+        decimal.textContent = `Decimal No : ${des}`;
     }
-     decimal.textContent = `Decimal No : ${des}`
+    
 }
-btn.addEventListener('click', function(e){
+btn.addEventListener('click', function (e) {
     e.preventDefault();
     convert()
 })
